477a291ffffff6802dca6de4451361e3fc563afb
jonvadney <jon.vadney@gmail.com>
2016-12-18 05:48:46 +0000

    Finalize touchscreen install

--- raspberry-setup/adafruit-touchscreen-setup.sh
+++ raspberry-setup/adafruit-touchscreen-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 curl -SLs https://apt.adafruit.com/add-pin | sudo bash
-
+sudo apt-get install -y --force-yes raspberrypi-bootloader adafruit-pitft-helper raspberrypi-kernel
+sudo adafruit-pitft-helper -t 35r
